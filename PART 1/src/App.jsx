b6fcab4ee4db060eaca9e1caf5ce2eeffaa256d1
Luovtyrell <luciakjhgfds@gmail.com>
2024-06-18 10:55:20 +0200

    Ex. 1.1.-1.2

---
+++ PART 1/src/App.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+Header.propTypes = {
+  course: PropTypes.string.isRequired,
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <p key={index}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+Content.propTypes = {
+  parts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      exercises: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+const Total = ({ total }) => {
+  return <p>Number of exercises {total}</p>;
+};
+
+Total.propTypes = {
+  total: PropTypes.number.isRequired,
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ];
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total total={total} />
+    </div>
+  );
+};
+
+export default App;
