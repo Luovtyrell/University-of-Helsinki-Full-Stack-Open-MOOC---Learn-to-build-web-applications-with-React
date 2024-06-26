b94a8e538a9ceb34a2baae99afa88b6817cb9844
Luovtyrell <luciakjhgfds@gmail.com>
2024-06-26 12:56:41 +0200

    Ex. 1.3 - 1.5

--- PART 1/src/App.jsx
+++ PART 1/src/App.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const Header = ({ course }) => {
   return <h1>{course}</h1>;
@@ -38,12 +38,22 @@ Total.propTypes = {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
+  const course = "Half Stack application development";
   const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
   ];
+
   const total = parts.reduce((sum, part) => sum + part.exercises, 0);
 
   return (
